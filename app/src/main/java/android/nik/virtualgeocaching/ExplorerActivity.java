d3bc473b047d9b8584cf27c2fce97651d07c0a10
ljaeger <eagle8520@gmail.com>
2017-04-19 18:52:43 +0200

    setcontentview were missing

--- app/src/main/java/android/nik/virtualgeocaching/ExplorerActivity.java
+++ app/src/main/java/android/nik/virtualgeocaching/ExplorerActivity.java
@@ -8,5 +8,6 @@ public class ExplorerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explorer);
     }
 }
