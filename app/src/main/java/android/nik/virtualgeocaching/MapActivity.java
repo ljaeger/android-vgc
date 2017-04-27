a5e91a666ff6c6f353fa255b7ff5f33240299e33
ljaeger <eagle8520@gmail.com>
2017-04-27 11:29:05 +0200

    Google Maps bugfix

--- app/src/main/java/android/nik/virtualgeocaching/MapActivity.java
+++ app/src/main/java/android/nik/virtualgeocaching/MapActivity.java
@@ -1,6 +1,8 @@
 package android.nik.virtualgeocaching;
 
+import android.content.pm.PackageManager;
 import android.os.PersistableBundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -8,6 +10,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -21,6 +24,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         super.onCreate(savedInstanceState);
         Chest selectedChest = (Chest) getIntent().getParcelableExtra("selectedChest");
         this.chest = selectedChest;
+
         setContentView(R.layout.activity_map);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -44,7 +48,25 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         mMap = googleMap;
 
         LatLng chestlatlong = chest.getPosition();
-        mMap.addMarker(new MarkerOptions().position(chestlatlong).title(chest.getChestID()));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(chestlatlong));
+        mMap.addMarker(new MarkerOptions().position(chestlatlong).title(chest.getChestID()).snippet(chest.getAdventurer().getName()).draggable(false));
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(chestlatlong)
+                .zoom(15)
+                .build();
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(chestlatlong));
+        //mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
     }
 }
